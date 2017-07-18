6b883e40fb1134873587e1edd3b95cffcb0df5c0
saleh alghusson <salehg@live.com>
2017-07-18 18:59:39 -0400

    almost done

---
+++ ArtNavigator/ArtNavigator/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  ArtNavigator
+//
+//  Created by saleh on 7/18/17.
+//  Copyright © 2017 saleh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    private let labelTitleColor = UIColor.blue
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ ArtNavigator/ArtNavigator/ListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListViewController.swift
+//  ArtNavigator
+//
+//  Created by saleh on 7/18/17.
+//  Copyright © 2017 saleh. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    private let labelTitleColor = UIColor.blue
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
