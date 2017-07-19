b045d5894aa997dcbe5ad2dc9c3953ca3bbf6e35
saleh alghusson <salehg@live.com>
2017-07-18 23:30:48 -0400

    submission.

--- ArtNavigator/ArtNavigator/MapViewController.swift
+++ ArtNavigator/ArtNavigator/MapViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapKit: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
