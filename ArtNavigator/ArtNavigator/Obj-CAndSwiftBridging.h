91baeb29bda3857ef9bb9d7a8ed5f94de1b624e4
saleh alghusson <salehg@live.com>
2017-07-26 18:54:33 -0400

    added toast framework, however, i don't like so i might remove in the next commit

---
+++ ArtNavigator/ArtNavigator/Obj-CAndSwiftBridging.h
@@ -0,0 +1,20 @@
+//
+//  Obj-CAndSwiftBridging.h
+//  ArtNavigator
+//
+//  Created by saleh on 7/26/17.
+//  Copyright © 2017 saleh. All rights reserved.
+//
+
+#ifndef Obj_CAndSwiftBridging_h
+#define Obj_CAndSwiftBridging_h
+
+// learned hpow to import framework and bridge from here:
+// https://stackoverflow.com/questions/24993752/os-x-framework-library-not-loaded-image-not-found
+// https://www.youtube.com/watch?v=kdX0HUTFWJo
+
+#import <EWToastAlerts/EWToastAlertView.h>
+#import <EWToastAlerts/EWToastAlerts.h>
+#import <EWToastAlerts/UIViewController+ext.h>
+
+#endif /* Obj_CAndSwiftBridging_h */
