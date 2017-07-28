b069ed376c5be1203eeadf61861efcac82fac2c0
saleh alghusson <salehg@live.com>
2017-07-27 23:22:52 -0400

    revising data

---
+++ ArtNavigator/ArtNavigator/Defaults.swift
@@ -0,0 +1,65 @@
+//
+//  Defaults.swift
+//  ArtNavigator
+//
+//  Created by saleh on 7/27/17.
+//  Copyright © 2017 saleh. All rights reserved.
+//
+
+import Foundation
+
+class Defaults {
+    static let supportedLanguages = ["ar":"العربية", "en":"English", "es":"Español", "fr":"Francês", "pt":"Português"]
+    
+    // source:
+    // https://swift3tutorials.com/swift-3-user-defaults/
+    static let defaults: UserDefaults = UserDefaults.standard
+    static var sliderValue: Int = 0
+    static var languageSelectedCode: String = ""
+    static var nightModeOn: Bool = true
+    
+    static let path = Bundle.main.path(forResource: languageSelectedCode, ofType: "lproj")
+    static let bundle = Bundle(path: path!)
+    
+    
+    private init(){}
+    
+    static func setDefaultsVaribles() {
+        sliderValue = defaults.integer(forKey: "sliderValue")
+        languageSelectedCode = defaults.string(forKey: "languageSelectedCode")!
+        nightModeOn = defaults.bool(forKey: "nightModeOn")
+        
+        if(languageSelectedCode.characters.count > 2){
+            print("languageSelectedCode is longer than 2: '\(languageSelectedCode)'")
+            languageSelectedCode = languageSelectedCode.substring(to:languageSelectedCode.index(languageSelectedCode.startIndex, offsetBy: 2))
+        }
+        if(languageSelectedCode != "ar" && languageSelectedCode != "en" && languageSelectedCode != "es" && languageSelectedCode != "fr" && languageSelectedCode != "pt"){
+            print("languageSelectedCode is none of the supported lanuages: '\(languageSelectedCode)'")
+            languageSelectedCode = "en"
+        }
+    }
+    static func register(defaultsArgument: [String: Any]) {
+        defaults.register(defaults: defaultsArgument)
+    }
+    static func set(key: String, value: Any) {
+        defaults.set(value, forKey: key)
+    }
+    static func get(key: String) -> String {
+        return defaults.string(forKey: key)!
+    }
+    static func printDefaults() {
+        print("printDefaults()")
+        print("sliderValue: \(sliderValue)")
+        print("languageSelectedCode: \(languageSelectedCode)")
+        print("nightModeOn: \(nightModeOn)")
+    }
+    // source:
+    // https://stackoverflow.com/a/27879342
+    static func getLocalizedString(key: String) -> String {
+        print("lang: \(languageSelectedCode)")
+        print("key: \(key)")
+        print("val: \(bundle?.localizedString(forKey: key, value: nil, table: nil ?? "nil") ?? "niiiiiil")")
+        return (bundle?.localizedString(forKey: key, value: nil, table: nil))!
+    }
+    
+}
--- ArtNavigator/ArtNavigator/SettingsViewController.swift
+++ ArtNavigator/ArtNavigator/SettingsViewController.swift
@@ -24,39 +24,31 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     
     @IBOutlet weak var buttonSave: UIButton!
     
-    //let tav =  EWToastAlertView(message: "Hey!", image: UIImage(named: "Cat Icon")!)
-
-    
-    let supportedLanguages = ["ar":"العربية", "en":"English", "es":"Español", "fr":"Francês", "pt":"Português"]
-    
-    // source: 
-    // https://swift3tutorials.com/swift-3-user-defaults/
-    let defaults: UserDefaults = UserDefaults.standard
-    var sliderValue: Int = 0
-    var languageSelectedCode: String = ""
-    var nightModeOn: Bool = true
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
         self.pickerLanguage.delegate = self
         self.pickerLanguage.dataSource = self
         
         buttonSave.layer.borderWidth = 0.7
         buttonSave.layer.cornerRadius = 5
         
-        defaults.register(defaults: ["sliderValue" : 5])
-        defaults.register(defaults: ["languageSelectedCode" : Locale.preferredLanguages[0]])
-        defaults.register(defaults: ["nightModeOn" : true])
+        Defaults.register(defaultsArgument: ["sliderValue" : 5])
+        Defaults.register(defaultsArgument: ["languageSelectedCode" : Locale.preferredLanguages[0]])
+        Defaults.register(defaultsArgument: ["nightModeOn" : true])
         
-        setDefaults()
-        printDefaults()
+        Defaults.setDefaultsVaribles()
+        Defaults.printDefaults()
         setViews()
     }
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(animated)
-        print("2")
-        setDefaults()
-        printDefaults()
+        
+        Defaults.setDefaultsVaribles()
+        Defaults.printDefaults()
         setViews()
     }
     
@@ -82,33 +74,33 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     // https://stackoverflow.com/a/36072699
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        let myView = UIView(frame: CGRect(x: 0, y: 0, width: pickerView.bounds.width - 30, height: 60))
         
+        let myView = UIView(frame: CGRect(x: 0, y: 0, width: pickerView.bounds.width - 30, height: 60))
         let myImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         
         var languageSelected: String
         switch row {
-        case 0:                             //The following are the ios language codes: en, fr, es, pt and ar
-            languageSelectedCode = "ar"
-            myImageView.image = UIImage(named: languageSelectedCode)
+        case 0:               //The following are the ios language codes: en, fr, es, pt and ar
+            Defaults.languageSelectedCode = "ar"
+            myImageView.image = UIImage(named: Defaults.languageSelectedCode)
         case 1:
-            languageSelectedCode = "en"
-            myImageView.image = UIImage(named: languageSelectedCode)
+            Defaults.languageSelectedCode = "en"
+            myImageView.image = UIImage(named: Defaults.languageSelectedCode)
         case 2:
-            languageSelectedCode = "es"
-            myImageView.image = UIImage(named: languageSelectedCode)
+            Defaults.languageSelectedCode = "es"
+            myImageView.image = UIImage(named: Defaults.languageSelectedCode)
         case 3:
-            languageSelectedCode = "fr"
-            myImageView.image = UIImage(named: languageSelectedCode)
+            Defaults.languageSelectedCode = "fr"
+            myImageView.image = UIImage(named: Defaults.languageSelectedCode)
         case 4:
-            languageSelectedCode = "pt"
-            myImageView.image = UIImage(named: languageSelectedCode)
+            Defaults.languageSelectedCode = "pt"
+            myImageView.image = UIImage(named: Defaults.languageSelectedCode)
         
         default:
-            languageSelectedCode = "xx"
+            Defaults.languageSelectedCode = "xx"
             myImageView.image = nil
         }
-        languageSelected = supportedLanguages[languageSelectedCode]!
+        languageSelected = Defaults.supportedLanguages[Defaults.languageSelectedCode]!
         let myLabel = UILabel(frame: CGRect(x: 60, y: 0, width: pickerView.bounds.width - 90, height: 60 ))
         //myLabel.font = UIFont(name:some, size: 18)
         myLabel.text = languageSelected
@@ -123,17 +115,17 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         
         switch row {    //The following are the ios language codes: en, fr, es, pt and ar
             case 0:
-                languageSelectedCode = "ar"
+                Defaults.languageSelectedCode = "ar"
             case 1:
-                languageSelectedCode = "en"
+                Defaults.languageSelectedCode = "en"
             case 2:
-                languageSelectedCode = "es"
+                Defaults.languageSelectedCode = "es"
             case 3:
-                languageSelectedCode = "fr"
+                Defaults.languageSelectedCode = "fr"
             case 4:
-                languageSelectedCode = "pt"
+                Defaults.languageSelectedCode = "pt"
             default:
-                languageSelectedCode = "xx"
+                Defaults.languageSelectedCode = "xx"
         }
     }
     
@@ -142,46 +134,38 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         let sliderValueFloat = Float(lroundf(sliderResults.value))
         sender.setValue(sliderValueFloat, animated: true)
         
-        sliderValue = Int(sliderValueFloat)
-        labelResultsNumbers.text = "\(sliderValue)"
+        Defaults.sliderValue = Int(sliderValueFloat)
+        labelResultsNumbers.text = "\(Defaults.sliderValue)"
     }
     @IBAction func switchChanged(_ sender: UISwitch) {
-        nightModeOn = sender.isOn
+        Defaults.nightModeOn = sender.isOn
     }
     
     @IBAction func buttonSavePressed(_ sender: Any) {
-        defaults.set(sliderValue, forKey: "sliderValue")
-        defaults.set(languageSelectedCode, forKey: "languageSelectedCode")
-        defaults.set(nightModeOn, forKey: "nightModeOn")
-        
-        let tav = EWToastAlertView()
-        tav.message = "Language will change upon restating the app"
-        tav.dismissTime = 3.0
-        /*
-        tav.shouldDismissWithTap
-        
-        tav.frameDuration = 0.1
-        tav.repeatAnimation = false
-        */
-        
-        tav.shouldDismissWithTap = true
-        tav.shouldDismissWithTime = true
-        tav.show()
-        //tav.dismiss() to Hide
+        print("-3")
+        let oldLanguageSelectedCode = Defaults.get(key: "languageSelectedCode")
+        print("-2")
+        Defaults.set(key: "sliderValue", value: Defaults.sliderValue)
+        Defaults.set(key: "languageSelectedCode", value: Defaults.languageSelectedCode)
+        Defaults.set(key: "nightModeOn", value: Defaults.nightModeOn)
+        print("-1")
+        Defaults.setDefaultsVaribles()
+        print("0")
+        if oldLanguageSelectedCode != Defaults.languageSelectedCode{
 
-
-
+            toast()
+        }
     }
     func setViews() {
-        labelResultsNumbers.text = "\(sliderValue)"
-        switchNightMode.isOn = nightModeOn
-        sliderResults.setValue(Float(sliderValue), animated: true)
+        labelResultsNumbers.text = "\(Defaults.sliderValue)"
+        switchNightMode.isOn = Defaults.nightModeOn
+        sliderResults.setValue(Float(Defaults.sliderValue), animated: true)
         
-        labelNightMode.text = NSLocalizedString("nightMode", comment: "")
-        labelResults.text = NSLocalizedString("resultsPerPage", comment: "")
-        buttonSave.setTitle(NSLocalizedString("save", comment: ""), for: .normal)
+        labelNightMode.text = Defaults.getLocalizedString(key: "nightMode")
+        labelResults.text = Defaults.getLocalizedString(key: "resultsPerPage")
+        buttonSave.setTitle(Defaults.getLocalizedString(key: "save"), for: .normal)
         
-        switch languageSelectedCode {
+        switch Defaults.languageSelectedCode {
             case "ar":
                 pickerLanguage.selectRow(0, inComponent: 0, animated: true)
             case "en":
@@ -198,23 +182,20 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         
     }
     
-    func setDefaults() {
-        sliderValue = defaults.integer(forKey: "sliderValue")
-        languageSelectedCode = defaults.string(forKey: "languageSelectedCode")!
-        nightModeOn = defaults.bool(forKey: "nightModeOn")
+    
+    func toast() {
+        print("TOAST")
+        let options: NSDictionary = [
+            kCRToastTextKey : Defaults.getLocalizedString(key: "restart"),
+            kCRToastTextAlignmentKey : NSTextAlignment.center.rawValue,
+            kCRToastBackgroundColorKey : UIColor.red,
+            kCRToastAnimationInTypeKey : CRToastAnimationType.gravity.rawValue,
+            kCRToastAnimationOutTypeKey : CRToastAnimationType.gravity.rawValue,
+            kCRToastAnimationInDirectionKey : CRToastAnimationDirection.left.rawValue,
+            kCRToastAnimationOutDirectionKey : CRToastAnimationDirection.right.rawValue
+        ]
         
-        if(languageSelectedCode.characters.count > 2){
-            print("languageSelectedCode is longer than 2: '\(languageSelectedCode)'")
-            languageSelectedCode = languageSelectedCode.substring(to:languageSelectedCode.index(languageSelectedCode.startIndex, offsetBy: 2))
-        }
-        if(languageSelectedCode != "ar" && languageSelectedCode != "en" && languageSelectedCode != "es" && languageSelectedCode != "fr" && languageSelectedCode != "pt"){
-            print("languageSelectedCode is none of the supported lanuages: '\(languageSelectedCode)'")
-            languageSelectedCode = "en"
-        }
-    }
-    func printDefaults() {
-        print("sliderValue: \(sliderValue)")
-        print("languageSelectedCode: \(languageSelectedCode)")
-        print("nightModeOn: \(nightModeOn)")
+        CRToastManager.showNotification(options: options as! [AnyHashable : Any], completionBlock: { () -> Void in
+        })
     }
 }
