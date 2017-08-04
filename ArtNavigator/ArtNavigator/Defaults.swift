2ca82671698fa87491b16736b87adbb163275aa0
saleh <almto3@hotmail.com>
2017-08-04 15:49:53 -0400

    new settings view

--- ArtNavigator/ArtNavigator/Defaults.swift
+++ ArtNavigator/ArtNavigator/Defaults.swift
@@ -8,15 +8,28 @@
 
 import Foundation
 
+enum SortBy : Int {
+    case title = 0
+    case category = 1
+    case artist = 2
+}
+
 class Defaults {
+    
     static let supportedLanguages = ["ar":"العربية", "en":"English", "es":"Español", "he":"עִברִית", "pt":"Português"]
     
     // source:
     // https://swift3tutorials.com/swift-3-user-defaults/
     static let defaults: UserDefaults = UserDefaults.standard
+    
+    static var sortByValue: Int = SortBy.title.rawValue
     static var sliderValue: Int = 5
-    static var languageSelectedCode: String = Locale.preferredLanguages[0][0..<2]
+    
     static var nightModeOn: Bool = true
+    static var largeTextOn: Bool = false
+    
+    static var languageSelectedCode: String = Locale.preferredLanguages[0][0..<2]
+    
     static var ranBefore: Bool = false
     
     static var path = Bundle.main.path(forResource: languageSelectedCode, ofType: "lproj")
@@ -25,14 +38,19 @@ class Defaults {
     private init() {}
     
     static func setDefaultsVaribles() {
-        
+        printDefaults()
         if defaults.object(forKey: "ranBefore") == nil && !ranBefore {
             firstTimeRun()
         }
         
+        sortByValue = defaults.integer(forKey: "sortByValue")
         sliderValue = defaults.integer(forKey: "sliderValue")
-        languageSelectedCode = defaults.string(forKey: "languageSelectedCode")!
+        
         nightModeOn = defaults.bool(forKey: "nightModeOn")
+        largeTextOn = defaults.bool(forKey: "largeTextOn")
+        
+        languageSelectedCode = defaults.string(forKey: "languageSelectedCode")!
+        
         ranBefore = defaults.bool(forKey: "ranBefore")
         
         //safety checks, should never enter them, but just in case
@@ -42,17 +60,23 @@ class Defaults {
         if(languageSelectedCode != "ar" && languageSelectedCode != "en" && languageSelectedCode != "es" && languageSelectedCode != "he" && languageSelectedCode != "pt"){
             languageSelectedCode = "en"
         }
+        printDefaults()
     }
     static func firstTimeRun() {
-        
+        print("**\nfirstTimeRun\n**\n")
         if supportedLanguages[languageSelectedCode] != nil {
             defaults.register(defaults: ["languageSelectedCode" : languageSelectedCode])
         }
         else{
             defaults.register(defaults: ["languageSelectedCode" : "en"])
         }
-        defaults.register(defaults: ["sliderValue" : 5])
-        defaults.register(defaults: ["nightModeOn" : true])
+        
+        defaults.register(defaults: ["sliderValue" : sliderValue])
+        defaults.register(defaults: ["sortByValue" : sortByValue])
+        //
+        defaults.register(defaults: ["nightModeOn" : nightModeOn])
+        defaults.register(defaults: ["largeTextOn" : largeTextOn])
+        //
         defaults.register(defaults: ["ranBefore" : true])
         defaults.synchronize()  
     }
@@ -66,12 +90,19 @@ class Defaults {
     static func get(key: String) -> String {
         return defaults.string(forKey: key)!
     }
+    //debugging purposes
     static func printDefaults() {
-        print("printDefaults()")
+        print("**\nprintDefaults()\n**\n")
         print("sliderValue: \(sliderValue)")
-        print("languageSelectedCode: \(languageSelectedCode)")
+        print("sortByValue: \(sortByValue)")
+        
         print("nightModeOn: \(nightModeOn)")
-        print("ranBefore: \(ranBefore)\n***\n")
+        print("largeTextOn: \(largeTextOn)")
+        
+        print("languageSelectedCode: \(languageSelectedCode)")
+        
+        print("ranBefore: \(ranBefore)")
+        print("**\nprintDefaults()\n**\n")
     }
     // source:
     // https://stackoverflow.com/a/27879342
